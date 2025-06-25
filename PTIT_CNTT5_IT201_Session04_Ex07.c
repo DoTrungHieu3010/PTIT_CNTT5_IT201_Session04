bba8e951386169d1213fa8f34fe257c74feaeb6a
DoTrungHieu3010 <dohieu30102006@gmail.com>
2025-06-25 15:51:28 +0700

    Add files via upload

--- PTIT_CNTT5_IT201_Session04_Ex07.c
+++ PTIT_CNTT5_IT201_Session04_Ex07.c
@@ -23,12 +23,10 @@ int main() {
         printf("Enter array number %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] == arr[j]) {
-                printf("Symmetrical pair(%d, %d)\n", arr[i], arr[j]);
-                found = 1;
-            }
+    for (int i = 0; i < n / 2; i++) {
+        if (arr[i] == arr[n - 1 - i]) {
+            printf("Symmetrical pair(%d, %d)\n", arr[i], arr[n - 1 - i]);
+            found = 1;
         }
     }
     if (!found) {
